c9a61258049696c739844cf655f93084649cab79
Nandhakumar <nknandhakumar64@gmail.com>
2025-05-28 14:33:56 +0530

    Love Letter app first code

---
+++ src/main/java/com/NKLoveLetterApp/LoveLetterApplication.java
@@ -0,0 +1,14 @@
+package com.NKLoveLetterApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveLetterApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
