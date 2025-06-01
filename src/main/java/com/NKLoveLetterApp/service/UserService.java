6ceb924be29ea6e54bc5bdf47774ac8bcf88dfa4
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-01 14:57:53 +0530

    created user service for auth

---
+++ src/main/java/com/NKLoveLetterApp/service/UserService.java
@@ -0,0 +1,30 @@
+package com.NKLoveLetterApp.service;
+
+import com.NKLoveLetterApp.model.User;
+import com.NKLoveLetterApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // user register
+    public String register(User user){
+        if (userRepository.findByUsername(user.getUsername())!= null){
+            return "User already exists";
+        }
+        userRepository.save(user);
+        return "Registered successfully!";
+    }
+
+    // user login
+    public String login(User user){
+        User existingUser = userRepository.findByUsername(user.getUsername());
+        if (!existingUser.getPassword().equals(user.getPassword())){
+            return "Invalid password";
+        }
+        return "Login successful!";
+    }
+}
