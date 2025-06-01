25a8087a193a9c31ab7cb768c92db539fc915d14
Nandhakumar <nknandhakumar64@gmail.com>
2025-06-01 14:46:55 +0530

    created user repository for auth

---
+++ src/main/java/com/NKLoveLetterApp/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.NKLoveLetterApp.repository;
+
+import com.NKLoveLetterApp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
+}
